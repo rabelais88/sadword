4a334b05f9c2cffe5eee250ea18815b1764d841d
Park Sungryeol <sungryeolp@gmail.com>
2017-12-28 03:18:10 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+/* sad word - node postgres + rest + jquery example web app */
+var fs = require("fs");
+var ejs = require("ejs");
+var app = require("express")();
+var http = require("http").Server(app);
+var bodyParser = require("body-parser");
+var uuid = require("uuid/v4");
+/* setting up postgreSQL */
+const pgp = require("pg-promise")(/* promise options */);
+
+const pgconfig = {
+  user: "postgres",
+  host: "localhost",
+  database: "sadword",
+  password: "devtest1",
+  port:5432,
+  /*
+    if it's heroku, replacce those with this:
+    connectionString : "postgresql://dbuser: ...."
+
+  */
+  poolSize:10, /* max number of clients */
+  idleTimeoutMillis:30000
+};
+
+/* connection is auto-configured from npm library, it isn't necessary */
+var db = pgp(pgconfig);
+
+app.set("view engine", "ejs");
+/* initialize bodyparser to build up RESTful app */
+app.use(bodyParser.urlencoded({ extended:false}));
+app.use(bodyParser.json());
+  
+http.listen(process.env.PORT || 3000,function(){
+  console.log("server is up at " + this.address().port);
+});
+  
+app.get("/",function(req,res){
+    db.any("SELECT writer_ip, article_content FROM sw_table LIMIT 10")
+    .then(function(data){
+      console.log("current rowlength:" + data.length);
+      /* data is provided in an array type made up of anonymous object elements */
+      console.log(data);
+      res.render("index.ejs", {data:data});
+    })
+    .catch(function(err){
+      console.log(err);
+    });
+});
+
