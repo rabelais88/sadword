ad26a55adb1709ecb47f2a8721878226ba01034f
Park Sungryeol <sungryeolp@gmail.com>
2017-12-30 05:58:17 +0900

    comment function added
    comment function added & asynchronization

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ const pgconfig = {
   user: "postgres",
   host: "localhost",
   database: "sadword",
-  password: "devtest1",
+  password: "testhotel1",
   port:5432,
   /*
     if it's heroku, replacce those with this:
@@ -35,17 +35,49 @@ http.listen(process.env.PORT || 3000,function(){
   console.log("server is up at " + this.address().port);
 });
   
-app.get("/",function(req,res){
-    db.any("SELECT writer_ip, article_content FROM sw_article LIMIT 10")
-    .then(function(data){
-      console.log("current rowlength:" + data.length);
-      /* data is provided in an array type made up of anonymous object elements */
-      console.log(data);
-      res.render("index.ejs", {data:data});
+/*
+  db structure:
+  create table sw_article (article_id serial primary key, writer_ip inet, article_content varchar(200), article_time timestamp);
+  create table sw_score (score_id serial primary key, scorer_ip inet, score boolean not null, article_id integer references sw_article(article_id));
+  create table sw_comment (comment_id serial primary key, commenter_ip inet, article_id integer references sw_article(article_id), comment_content varchar(150));
+*/
+
+function getArticle(beginRow,endRow,callback){
+  var rowLength = endRow - beginRow;
+  db.any("SELECT writer_ip, article_content, article_id FROM sw_article LIMIT " + rowLength + " OFFSET " + beginRow)
+  .then(function(data){
+    var dataArticle = [];
+    //console.log("current rowlength:" + data.length);
+    /* data is provided in an array type made up of anonymous object elements */
+    data.forEach(function(elArticle){
+      dataArticle[elArticle.article_id] = elArticle;
+      dataArticle[elArticle.article_id].comment = [];
+    });
+    db.any("SELECT * FROM sw_comment WHERE article_id BETWEEN " + beginRow + " AND " + endRow)
+    .then(function(dataComment){
+      dataComment.forEach(function(elComment){
+        var newcomment = {commenter_ip:elComment.commenter_ip,
+                          comment_content:elComment.comment_content,
+                          comment_id:elComment.comment_id};
+        dataArticle[elComment.article_id].comment.push(newcomment);
+      });
+      console.log(dataArticle);
+      callback(dataArticle);
     })
     .catch(function(err){
       console.log(err);
     });
+  })
+  .catch(function(err){
+    console.log(err);
+  });
+}
+
+app.get("/",function(req,res){
+  /* EVERY postgres action is async. => always use callback to print out the result */
+  getArticle(0,10,function(dataRes){
+    res.render("index.ejs", {articles:dataRes});
+  });
 });
 
 app.get("/write",function(req,res){
